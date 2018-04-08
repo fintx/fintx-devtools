3b4764cf6a11b7a5e055342592ea4fd77bbc14b8
bluecreator <qiang.x.wang@gmail.com>
2018-04-08 22:40:51 +0800

    add generator implement classes

---
+++ src/main/java/com/fintx/generator/mapper/Mybatis3XmlMapperGenerator.java
@@ -0,0 +1,47 @@
+/**
+ *  Copyright 2018 FinTx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fintx.generator.mapper;
+
+import com.fintx.generator.Context;
+import com.fintx.generator.xml.XmlFile;
+
+import java.util.List;
+
+/**
+ * @author bluecreator(qiang.x.wang@gmail.com)
+ *
+ */
+public class Mybatis3XmlMapperGenerator implements XmlSqlMapperGenerator {
+
+    /* (non-Javadoc)
+     * @see com.fintx.generator.xml.XmlGenerator#getTargetFile()
+     */
+    @Override
+    public List<XmlFile<?>> getTargetFile() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see com.fintx.generator.file.FileGenerator#generate(com.fintx.generator.Context)
+     */
+    @Override
+    public void generate(Context context) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/com/fintx/generator/repository/SpringMybatisRepositoryGenerator.java
@@ -0,0 +1,47 @@
+/**
+ *  Copyright 2018 FinTx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fintx.generator.repository;
+
+import com.fintx.generator.Context;
+import com.squareup.javapoet.JavaFile;
+
+import java.util.List;
+
+/**
+ * @author bluecreator(qiang.x.wang@gmail.com)
+ *
+ */
+public class SpringMybatisRepositoryGenerator implements JavaRepositoryGenerator {
+
+    /* (non-Javadoc)
+     * @see com.fintx.generator.java.JavaGenerator#getTargetFile()
+     */
+    @Override
+    public List<JavaFile> getTargetFile() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see com.fintx.generator.file.FileGenerator#generate(com.fintx.generator.Context)
+     */
+    @Override
+    public void generate(Context context) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
